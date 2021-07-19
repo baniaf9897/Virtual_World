e21683d7f24bbce015fc90933364cfd0436180c1
Fabian Töpfer <fabian.toepfer@tu-dresden.de>
2021-07-19 18:36:32 +0200

    adding dynamic / random rules

--- Assets/Main/WorldMgr.cs
+++ Assets/Main/WorldMgr.cs
@@ -9,15 +9,38 @@ using UnityEditor;
     CellularAutomatum m_CellularAutomatumManager;
     public bool trigger = false;
 
-    float updateRateSec = 0.005f;
+    float updateRateSec = 0.01f;
     float time = 0.0f;
+
+    float shaderTime = 0.0f;
+
+
+    public int ruleSurviveLow = 0;  
+    public int ruleSurviveHigh  = 0;
+
+    public int ruleDieLow = 0;
+    public int ruleDieHigh = 0;
+
+    public  int ruleBornLow = 0;
+    public int ruleBornHigh = 0;
+
+    bool newRules = false;
+
     void Update()
     {
         time += Time.deltaTime;
+        shaderTime += Time.deltaTime;
         if(time > updateRateSec) {
-            m_CellularAutomatumManager.m_generation += time;
+            m_CellularAutomatumManager.m_generation += time*5.0f;
+
+            if(m_CellularAutomatumManager.m_generation >= m_CellularAutomatumManager.height)
+            {
+                m_CellularAutomatumManager.m_generation = 0.0f;
+                newRules = true;
+            }
+
             m_CellularAutomatumManager.generateNewGen = true;
-             time = 0.0f;
+            time = 0.0f;
         };
 
 
@@ -66,16 +89,45 @@ using UnityEditor;
     {         
         m_CellularAutomatumManager = transform.GetComponent<CellularAutomatum>();
        
-        if(m_CellularAutomatumManager.m_generation == 0)
+        if(m_CellularAutomatumManager.m_generation == 0 && !newRules)
             m_CellularAutomatumManager.InitTexture();
+
+
+        if(newRules)
+        {
+              ruleSurviveLow = Random.Range(0, 8);
+              ruleSurviveHigh = ruleSurviveLow + Random.Range(0, 8 - ruleSurviveLow);
+
+              ruleDieLow = Random.Range(0, 8); //rand(time, 132) * 8.0;
+              ruleDieHigh = ruleDieLow + Random.Range(0, 8 - ruleDieLow);
+
+              ruleBornLow = Random.Range(0, 8); ;// rand(time, _Time) * 8.0;
+              ruleBornHigh = ruleBornLow + Random.Range(0, 8 - ruleBornLow);
+
+            newRules = false;
+
+        }
+
 
         CA.SetTexture(0, "Automatum", m_CellularAutomatumManager.automatum);
         CA.SetInt("width", m_CellularAutomatumManager.width);
         CA.SetInt("depth", m_CellularAutomatumManager.depth);
         CA.SetInt("height", m_CellularAutomatumManager.height);
         CA.SetFloat("currentLayer", m_CellularAutomatumManager.m_generation);
+        CA.SetFloat("_Time", shaderTime);
+
+       
+
+        CA.SetInt("ruleSurviveLow", ruleSurviveLow);
+        CA.SetInt("ruleSurviveHigh", ruleSurviveHigh);
+        CA.SetInt("ruleDieLow", ruleDieLow);
+        CA.SetInt("ruleDieHigh", ruleDieHigh);
+        CA.SetInt("ruleBornLow", ruleBornLow);
+        CA.SetInt("ruleBornHigh", ruleBornHigh);
 
         ComputeCA();        
+
+            
     }
     public Texture3D CreateTexture3D()
     {
